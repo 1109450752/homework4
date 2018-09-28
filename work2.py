d867e1d58486d619e8802b7269a0aa73fc72d086
root <root@localhost.localdomain>
2018-09-27 21:01:24 -0400

    a

--- work2.py
+++ work2.py
@@ -1,10 +1,11 @@
 a=10000 
-sum1=10000
+sum1=0
 for i in range(10):
     a=round(a*(1+0.05),2)
-    #print(a)
-    if i<3 :
-        sum1+=a
-print(a,sum1)
+print(a)
+for i in range(4):
+   sum1 = sum1 + a
+   a=round(a*(1+0.05),2)
+   print(sum1)
 
 
